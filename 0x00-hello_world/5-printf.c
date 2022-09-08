fb898bf95405b6c6ed01ca0f4d892813f97b3baf
izy4eze <chibuezeokorieone@gmail.com>
2022-09-08 13:30:50 -0700

    updated task 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
- *The program that prints exactly with proper grammar, but the outcome is a piece of art
+ * main - Entry point
+ * This programe print the szie of various types of compile task
  *
- *And it is return a 0 as output
- *Return: Always 0 (success)
+ * return  a 0 as outpu
+ * Return: Always 0 (success)
  */
 
 int main(void)
 
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 
 }
