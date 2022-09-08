3344a0c0ce7ebdccac32e2c10ba2fd63b297601d
izy4eze <chibuezeokorieone@gmail.com>
2022-09-08 13:47:29 -0700

    Task 6 done

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>                                                                                                                 
+
+/**                                                                                                                                
+ *
+ *  * main - Entry point                                                                                                           
+ *  This programe print the szie of various types of compile task    
+ *
+ *  return  a 0 as output       
+ *  Return: Always 0 (success)                                                                                                     
+ */ 
+
+int main(void)                                                                                                                     {                                                                                                                                
+	printf("Size of a char: %d byte(s)\n", sizeof(char));                                                                     
+	printf("Size of an int: %d byte(s)\n", sizeof(int));                                      
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));                                                             
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));                                           
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));                                                                  
+	return (0);   
+
+}
