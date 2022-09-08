2d41bbecd9004ad299abd4b7cbe8cbaf40647feb
izy4eze <chibuezeokorieone@gmail.com>
2022-09-08 11:21:30 -0700

    Task 4 updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+
 /**
  *
  *  * main - Entry point
@@ -10,7 +12,12 @@
  *
  *     */
 
-int main(void){
-	puts("Programming is like building a multilingual puzzle\n");
-	return (0);
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
