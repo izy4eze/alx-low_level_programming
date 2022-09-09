8d84a26d10fd252c11e67114782be74c733693d3
izy4eze <chibuezeokorieone@gmail.com>
2022-09-09 00:55:39 -0700

    Task 16 lastest

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ *main - Entry point 
  * This is a script prints the size of various size on computer
  * Return: Always 0 (Sucess)
  */
@@ -12,7 +12,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-	return 0;
-     	
-
+	return (0);
 }
