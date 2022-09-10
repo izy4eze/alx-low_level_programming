228dc49a3dd6c04202f669c136f949f8490aa9a6
izy4eze <chibuezeokorieone@gmail.com>
2022-09-09 23:38:02 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+* This programe prints all single digits 
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
+
+	return (0);
+}
