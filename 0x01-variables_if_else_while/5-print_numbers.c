ddf3d2a36057b3b19059211efdfc071339734921
izy4eze <chibuezeokorieone@gmail.com>
2022-09-09 23:18:10 -0700

    Task 5 done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+* This program prints all digit nummber
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	print('\n');
+
+	return (0);
+}
