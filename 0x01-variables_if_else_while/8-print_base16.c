810ef0defde0631ebc2d934cbcf5f96128c4706e
izy4eze <chibuezeokorieone@gmail.com>
2022-09-10 02:18:06 -0700

    corrected 4

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,13 +11,13 @@
 int main(void)
 {
 	int d;
-	int low;
+	char low;
 
 	for (d = '0'; d <= '9'; d++)
 		putchar(d);
 	for (low = 'a'; low <= 'f'; low++);
-	
 	putchar(low);
+	
 	putchar('\n');
 
 	return (0);
