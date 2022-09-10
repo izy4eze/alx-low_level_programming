bf75383dff0a3324ceca8bb7c877e01682a26554
izy4eze <chibuezeokorieone@gmail.com>
2022-09-10 14:08:36 -0700

    lasted updated

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,14 @@ int main(void)
 {
 	int d;
 
-	for ( d= '0'; d <= '9'; d++)
+	for (d = '0'; d <= '9'; d++)
 	{
 		putchar(d);
-		if ( d != '9')
+		if (d != '9')
 		{
-			putchar(',');			
+			putchar(',');
+			putchar('');
+
 		}
 	}
 	putchar('\n');
