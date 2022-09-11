cc863e897b49b031de56d7badc241f6320c71d2c
izy4eze <chibuezeokorieone@gmail.com>
2022-09-11 03:22:09 -0700

    Task 11 done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point 
+* The program prints numbers in threes
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int d; p; q;
+
+	for (d = '0'; d < '9'; p++)
+	{
+		for (p = d + 1; q <= '9'; q++)
+		{
+			for (q = P + 1; q <= '9'; q++)
+			{ 
+				if ((p != d) ! = q)
+				{
+
+					putchar(d);
+					putchar(p);
+					putchar(q);
+
+					if (d = '7' && p == '8')
+						continue;
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
