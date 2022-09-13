9e410dbd56a947985e8a32ef6c4c59e50947adc3
izy4eze <chibuezeokorieone@gmail.com>
2022-09-12 23:19:14 -0700

    Task o done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.c"
+#include "_putchar.c"
+
+/**
+* main - Entry point
+* The program prints alphabet in lowwercase
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	_putchar("_putchar\n");
+
+	return (0);
+}
