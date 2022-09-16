31bbd0eb02151acfd984757a35f4d3ed6066af37
izy4eze <chibuezeokorieone@gmail.com>
2022-09-16 10:43:33 -0700

    Task 0 last corrected

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,10 +2,10 @@
 #define MAIN_H_INCLUDED
 #include <stdio.h>
 
-int _putchar(char);
-int _putchar(char);
-int _isdigit(int);
-int mul(int, int);
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
