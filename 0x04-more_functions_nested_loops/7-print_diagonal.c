1acfef27b6af770284207e0560efb3c34a071e25
izy4eze <chibuezeokorieone@gmail.com>
2022-09-17 02:14:28 -0700

    Task 7 done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+* print_square - prints a square. followed by new line
+* @size: size of the square
+*/
+
+void print_square(int  n)
+{
+	if (size  <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int  i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; i < size; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+
+
+}	
