ca959c4027f68907e3929a4253d4383566ab1e28
izy4eze <chibuezeokorieone@gmail.com>
2022-09-16 22:53:18 -0700

    lasted update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 
 /**
-* print_most_numbers- checks for a digits (0 through 9)
+* print_numbers- checks for a digits (0 through 9)
 * followed by a new line
 * Return: Always 0
 */
