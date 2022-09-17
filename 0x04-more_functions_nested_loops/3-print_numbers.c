24a55908c06181538fda7b3a2b482810f451446b
izy4eze <chibuezeokorieone@gmail.com>
2022-09-16 22:42:34 -0700

    task three corrected

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 
 /**
-* print_numbers - a function that prints the numbers, from 0 to 9
+* print_most_numbers- checks for a digits (0 through 9)
 * followed by a new line
 * Return: Always 0
 */
