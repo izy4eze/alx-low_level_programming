f6a99bbc1d88d44a4de938625cf62758aaee9e58
izy4eze <chibuezeokorieone@gmail.com>
2022-09-22 12:54:15 -0700

    Task 1

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,9 +14,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int i, j, k, l, m, n;
 
 	for (i = 0; n1[i]; i++)
-		for (j = 0; n2[j]; j++)
-			if (i > size_r || j > size_r)
-				return (0);
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		return (0);
 	m = 0;
 
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
@@ -26,6 +28,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			n += n1[i] - '0';
 		if (j >= 0)
 			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
 		{
 			break;
 		}
