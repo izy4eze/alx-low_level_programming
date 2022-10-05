5d23b15611d903f350ca56ac25d54706e8095df5
izy4eze <chibuezeokorieone@gmail.com>
2022-10-05 05:48:09 -0700

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,6 +3,7 @@
 /**
 * main - finds and prints the sum of the even-valued terms
 * followed by a new line
+* This is for task 15
 * Return: Always 0 (Success)
 */
 int main(void)
--- 0x02-functions_nested_loops/READ.md
+++
@@ -1,1 +0,0 @@
-Write a function that prints the 9 times table, starting with 0
