1ef58e36aaf58c0fcbc797ff64d630b40257bf0c
izy4eze <chibuezeokorieone@gmail.com>
2022-10-11 14:12:13 -0700

    Task 0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
 * main - Entry point
@@ -6,19 +8,18 @@
 * Return: Always 0 (Success)
 */
 
-void positive_or_negative(int i)
+void positive_or_negative(void)
 {
-	/* my if statement */
-	if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 	else
-	{
-		printf("%d is negative\n", i);
-	}
+		printf("%d is negative\n", n);
+	return (0);
 }
