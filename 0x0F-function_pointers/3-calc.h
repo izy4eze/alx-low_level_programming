2651837fd20687d77ca58e419d306fb41b2fb615
izy4eze <chibuezeokorieone@gmail.com>
2022-10-12 22:59:32 -0700

    Task 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -10,7 +10,6 @@
 typedef struct op
 {
 	char *op;
-	
 	int (*f)(int a, int b);
 } op_t;
 
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,34 +1,56 @@
 #include "3-calc.h"
-#include <stdlib.h>
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
 /**
-
-* get_op_func - Selects the correct function to perform
-
-*               the operation asked by the user.
-* @s: The operator passed as argument.
-* Return: A pointer to the function corresponding
-
-*         to the operator given as a parameter.
-
+* op_add - Returns the sum of two numbers.
+* @a: The first number.
+* @b: The second number.
+* Return: The sum of a and b.
 */
 
-int (*get_op_func(char *s))(int, int)
+int op_add(int a, int b)
 {
-	op_t ops[] = {
+	return (a + b);
+}
 
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+/**
+* op_mul - Returns the product of two numbers.
+* @a: The first number.
+* @b: The second number.
+* Return: The product of a and b.
+*/
 
-	};
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
 
-	int i = 0;
-	
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
-		i++;
-	return (ops[i].f);
+/**
+* op_div - Returns the division of two numbers.
+* @a: The first number.
+* @b: The second number.
+* Return: The quotient of a and b.
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - Returns the remainder of the division of two numbers.
+* @a: The first number.
+* @b: The second number.
+* Return: The remainder of the division of a by b.
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
 
