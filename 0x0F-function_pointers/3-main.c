a6d24ccf849b362a6b243c4ba6c317971d6b0614
izy4eze <chibuezeokorieone@gmail.com>
2022-10-13 09:21:08 -0700

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,49 +1,41 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
-* main - Prints the result of simple operations.
-* @argc: The number of arguments supplied to the program.
-* @argv: An array of pointers to the arguments.
+* main - check the code for Holberton School students.
+* @argc: argument count. 
+* @argv: argument vector.
 * Return: Always 0.
 */
 
 int main(int argc, char *argv[])
 {
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
+	int a, b;
+	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
+
 	}
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
-
-	func  = get_op_func(argv[2]);
-
-	if (!func)
+	if (argv[2][1])
 	{
-		printf("Erro\n");
+		printf("Error\n");
 		exit(99);
 	}
 
-	o = *argv[2];
+	operation = get_op_func(argv[2]);
 
-	if ((o == '/' || o == '%') && arg2 == 0)
+	if (operation == NULL)
 	{
-		printf("Erro\n");
-			exit(100);
+		printf("Error\n");
+		exit(99);
 	}
 
-	result  = func(arg1, arg2);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-	printf("%d\n", result);
-
+	printf("%d\n", operation(a, b));
 	return (0);
 }
-
