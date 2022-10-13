02ec0ee114bb16a88e4bbdde3b4ec31d11cc774a
izy4eze <chibuezeokorieone@gmail.com>
2022-10-13 10:10:01 -0700

    Task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-* print_opcodes - print the opcodes of this program 
+* print_opcodes - print the opcodes of this program
 * @a: address of the main function
 * @n: number of bytes to print
 * Return: void
@@ -26,7 +26,6 @@ void print_opcodes(char *a, int n)
 * main - prints the opcodes of its own main function
 * @argc: number of arguments passed to the function
 * @argv: array of pointers to arguments
- 
 * Return: always O
 */
 
