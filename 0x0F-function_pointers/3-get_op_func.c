6f5942b7373a5060e91a167ca1cf096337e99411
izy4eze <chibuezeokorieone@gmail.com>
2022-10-13 00:13:34 -0700

    Task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -24,9 +24,14 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
 		i++;
-	return (ops[i].f);
+	}
+
+	return (NULL);
 
 }
 
