19dd1db63530a52a952a20bf48b8ce47ad2ed77f
izy4eze <chibuezeokorieone@gmail.com>
2022-10-16 12:57:11 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,34 +8,26 @@
 * Return: Always 0 (success)
 */
 
-void main(void)
+int main(void)
 {
-	int  i;
+	int num;
 
-	for (i = 1; i <= 100; i++)
+	for (num = 1; num <= 100; ++num)
 	{
-		if (i % 3 == 0 &&  i % 5 != 0)
-		{
+		if (num % 3 == 0 && !(num % 5 == 0))
 			printf("Fizz");
-		}
-		else if (i % 5 == 0 && i % 3 != 0)
-		{
+		else if (num % 5 == 0 && !(num % 3 == 0))
 			printf("Buzz");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
+		else if (num % 3 == 0 && num % 5 == 0)
 			printf("FizzBuzz");
-		}
-		else if (i == 1)
-		{
-			printf("%d", i);
-		}
 		else
-		{
-			printf("%d", i);
-		}
+			printf("%d", num);
+
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
 	}
-	printf('\n');
 
 	return (0);
 }
