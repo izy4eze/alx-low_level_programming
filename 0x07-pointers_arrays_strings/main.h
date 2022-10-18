146c726ff9257fd0344009847d70a71b7c8184bc
izy4eze <chibuezeokorieone@gmail.com>
2022-10-18 07:49:15 -0700

    Main.h file corrected

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,14 +2,15 @@
 #define MAIN_H_INCLUDED                                                                                                                                
 #include <stdio.h>                                                                                                                                     
                                                                                                                                                       
-int _putchar(char c);                                                                                                                                int _isupper(int c);  
-int _isdigit(int c);                                                                                                                                 int mul(int a, int b);                                                                                                                               
-void print_numbers(void);                                                                                                                            void print_most_numbers(void);                                                                                                                     
-void more_numbers(void);                                                                                                                             void print_line(int n);                                                                                                                          
-void print_diagonal(int n);                                                                                                                          
-void print_square(int size);                                                                                                                         void print_triangle(int size);                                                                                                                      
-void print_number(int n);           
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+int _putchar(char c);
 void set_string(char **s, char *to);
                                                                                                                                                       
 #endif              
