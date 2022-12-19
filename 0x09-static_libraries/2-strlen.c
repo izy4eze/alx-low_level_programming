e55bcfef86a4f7089d89978582bbe0bf7965f70b
izy4eze <chibuezeokorieone@gmail.com>
2022-12-19 11:29:52 -0800

    Task 0 corrected

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,17 +1,21 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _strlen - Function that returns the length of a string
- *
- * @s: this is the input string
- *
- * Return: Lenngth of the string
- */
+ * _strlen - Function for counting the length of string
+ * @s: a pointer for counting the strting lenth
+ * Author: Sammykingx
+ * Return: int i
+*/
+
 int _strlen(char *s)
 {
-	int index;
+	int i = 0;
 
-	for (index = 0; s[index] != '\0' ; index++)
-		;
-	return (index);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
 }
