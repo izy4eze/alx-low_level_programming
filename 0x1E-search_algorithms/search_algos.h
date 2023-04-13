63047725c3fa5aefd9173438ed4e732e5514c562
izy4eze <chibuezeokorieone@gmail.com>
2023-04-13 09:45:07 -0700

    Task corrected

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,63 +12,54 @@
  * @index: Index of the node in the list
  * @next: Pointer to the next node
  *
- * Description: singly linked node list structure
- * for Holberton Project
+ * Description: singly linked list node structure
+ * for Holberton project
  */
-
 typedef struct listint_s
 {
 	int n;
 	size_t index;
 	struct listint_s *next;
-
 } listint_t;
 
 
 /*
  * Function that searches for a value in an
- * array of integer using the linear search algorithm
+ * array of integers using the Linear search algorithm
  */
-
 int linear_search(int *array, size_t size, int value);
-
 
 /*
  * Function that searches for a value in a sorted array
  * of integers using the Binary search algorithm
  */
-
 int binary_search(int *array, size_t size, int value);
 
 /*
- *  Function that searches for a value in a
- *  sorted array of integers using the Jump search algorithm
+ * Function that searches for a value in a
+ * sorted array of integers using the Jump search algorithm
  */
-
-int jump_search(int *array; size_t; int value):
+int jump_search(int *array, size_t size, int value);
 
 /*
  * Function that searches for a value in a sorted array of
  * integers using the Interpolation search algorithm
  */
-
 int interpolation_search(int *array, size_t size, int value);
 
 /*
- * Function that searches for a value  in a sorted way
- * of integer using the Exponential search algorithm
+ * Function that searches for a value in a sorted array
+ * of integers using the Exponential search algorithm
  */
-
 int exponential_search(int *array, size_t size, int value);
 
-/* function that searches for a value in a sorted list
- * of integer the Jump search algorithm in a singly linked list
+/*
+ * Function that searches for a value in a sorted list
+ * of integers using the Jump search algorithm in a singly linked list
  */
-
 listint_t *jump_list(listint_t *list, size_t size, int value);
 
-/*  print_list - Prints the content of a listint_t */
-
+ /* print_list - Prints the content of a listint_t */
 void print_list(const listint_t *list);
 
 /* create_list - Creates a single linked list */
@@ -78,4 +69,3 @@ listint_t *create_list(int *array, size_t size);
 void free_list(listint_t *list);
 
 #endif /* SEARCH_H */
- 
